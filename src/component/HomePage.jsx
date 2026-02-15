309ffedeabc24eba08b8502dad93a794af63c139
Ayushman Gupta <ayushmangupta@Ayushmans-MacBook-Air.local>
2026-02-15 13:06:09 +0530

    Home Page Created

---
+++ src/component/HomePage.jsx
@@ -0,0 +1,47 @@
+import "../css/HomePage.css";
+import HomePageLogo from '../assets/Home_Page_Logo.jpeg'
+function HomePage() {
+  return (
+    <div className="homepage-wrapper">
+      <div className="container-fluid">
+        <div className="row min-vh-100">
+          {/* Left Section */}
+
+          <div className="col-md-7 d-flex justify-content-center align-items-center right-section">
+            <img
+              src={HomePageLogo}
+              alt="Finance Tracker Illustration"
+              className="homepage-image img-fluid"
+            />
+          </div>
+
+          {/* Right Section */}
+
+          <div className="col-md-5 d-flex flex-column justify-content-center align-items-start left-section">
+            <h1 className="app-title mb-3">Finance Tracker</h1>
+
+            <p className="app-subtitle mb-4">
+              Track your expenses, manage your savings, and control your
+              financial future effortlessly.
+            </p>
+
+            <div className="login-signup-button">
+              <div className="button-group rounded-pill">
+                <button className="btn btn-primary custom-btn mb-3">
+                  Sign Up
+                </button>
+              </div>
+
+              <div className="button-group rounded-pill">
+                <button className="btn btn-primary custom-btn mb-3">
+                  Sign In
+                </button>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+export default HomePage;
