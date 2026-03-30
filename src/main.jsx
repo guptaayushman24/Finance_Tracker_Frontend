fc3ccfb583600de106b6052693f3eb305ea9fcc0
Ayushman Gupta <ayushmangupta@Ayushmans-MacBook-Air.local>
2026-03-30 22:21:47 +0530

    deploymemnt issue resolved

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,7 @@ import { StrictMode} from 'react'
 import ReactDOM from "react-dom/client";
 import { createRoot } from 'react-dom/client'
 import { Provider } from "react-redux";
-import { store } from "./redux/store";
+import store from './redux/Store.js';
 import App from './App.jsx'
 import 'bootstrap/dist/css/bootstrap.min.css';
 
